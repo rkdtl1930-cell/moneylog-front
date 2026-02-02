1b114aa51342c26afd45e6b260b852d1419991e2
DESKTOP-G407F77\it <am_o_o@naver.com>
2026-02-02 18:02:24 +0900

    401/404 페이지 업데이트

--- src/components/pages/error/NotFound.jsx
+++ src/components/pages/error/NotFound.jsx
@@ -1,15 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import './ui.css'
 
-export default function NotFound(){
-  return(<>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 text-center">
-          <span className="display-1">404</span>
-          <div className="mb-4 lead">주소가 변경되었거나 페이지를 찾지 못했습니다</div>
-          <Link to='/home' className="btn btn-link">Back to Home</Link>
+function NotFound(){
+  const [currentImage, setCurrentImage] = useState(1);
+  
+  useEffect(() => {
+    const imageSequence = [1, 2, 1, 3]; 
+    let index = 0;
+    
+    const interval = setInterval(() => {
+      index = (index + 1) % imageSequence.length;
+      setCurrentImage(imageSequence[index]);
+    }, 800);
+    
+    return () => clearInterval(interval);
+  }, []);
+  
+  return (
+    <div className="container uiBox notFound">
+      <div>
+        <h1>404</h1>
+        <div className="char-container">
+          <img 
+            src={`/images/sub/ui-char0${currentImage}.svg`} 
+            alt="" 
+            className="char"
+          />
         </div>
+        <h2>페이지를 찾을 수 없습니다.</h2>
+        <p>페이지 주소가 잘못 입력되었거나, 주소가 변경 또는 삭제되어<br/>
+        요청하신 페이지를 찾을 수 없습니다.</p>
+        <a href="/">메인으로</a>
       </div>
     </div>
-  </>)
+  )
 }
+export default NotFound;
--- src/components/pages/error/UnAuthorized.jsx
+++ src/components/pages/error/UnAuthorized.jsx
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function UnAuthorized(){
-  return(<>
-    <div className="container">
-      <div className="row">
-        <div className="col-md-12 text-center">
-          <span className="display-1">401</span>
-          <div className="mb-4 lead">권한이 없습니다. 이 주소로 접근이 거부되었습니다.</div>
-          <Link to='/home' className="btn btn-link">Back to Home</Link>
+function UnAuthorized(){
+  const [currentImage, setCurrentImage] = useState(1);
+   useEffect(() => {
+      const imageSequence = [1, 2, 1, 3]; 
+      let index = 0;
+      
+      const interval = setInterval(() => {
+        index = (index + 1) % imageSequence.length;
+        setCurrentImage(imageSequence[index]);
+      }, 800);
+      
+      return () => clearInterval(interval);
+    }, []);
+  return (
+   <div className="container uiBox notFound">
+      <div>
+        <h1>401</h1>
+        <div className="char-container">
+          <img 
+            src={`/images/sub/ui-char0${currentImage}.svg`} 
+            alt="" 
+            className="char"
+          />
         </div>
+        <h2>로그인이 필요한 페이지입니다.</h2>
+        <p>로그인 후 이용해주세요..</p>
+        <a href="/login">로그인으로</a>
       </div>
     </div>
-  </>)
+  )
 }
+export default UnAuthorized;
